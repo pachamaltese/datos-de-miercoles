7336bd0435b931416513023c03c00b8817e3cef0
Pachamaltese <mvargas@dcc.uchile.cl>
2019-11-06 10:47:35 -0300

    semana 44

--- meta/guardar-tweets.R
+++ meta/guardar-tweets.R
@@ -4,8 +4,8 @@ pacman::p_load(rtweet, httpuv, dplyr, lubridate, glue)
 data <- search_tweets("#datosdemiercoles", n = 1000, type = "recent", retryonratelimit = TRUE, include_rts = TRUE)
 
 assign(glue("semana", week(Sys.Date() - days(1))),
-       data %>% 
-         filter(created_at > Sys.Date() - days(8) & 
+       data %>%
+         filter(created_at > Sys.Date() - days(8) &
                   created_at < Sys.Date()
          )
 )
@@ -13,4 +13,4 @@ assign(glue("semana", week(Sys.Date() - days(1))),
 try(dir.create("meta"))
 try(dir.create(glue("meta/{year(Sys.Date())}")))
 
-save(semana42, file = glue("meta/{year(Sys.Date())}/{Sys.Date() - days(1)}.rda"), compress = "xz")
+save(semana44, file = glue("meta/{year(Sys.Date())}/{Sys.Date() - days(1)}.rda"), compress = "xz")
