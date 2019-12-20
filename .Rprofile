23f31d101cbe344de20efd7e58782865db83da0d
Pachá <mvargas@dcc.uchile.cl>
2019-12-20 19:09:45 -0300

    semana 50

--- .Rprofile
+++ .Rprofile
@@ -1,2 +1,2 @@
-api_key="k2dsGWJSi2VCANuTNUdYt8NGV"
-api_secret_key="dlURjTJml0vCMZjyCpYnRC9xNgEzE848eeC3SDAmzqk8IPtqiW"
+api_key="UUmYdaih9qa5FliBckhEoCx7J"
+api_secret_key="Mk3muS0rK28jdPcsBpuy8DOfstr6T7H2w1qKbLMM1OG2kcL4hB"
--- meta/guardar-tweets.R
+++ meta/guardar-tweets.R
@@ -1,6 +1,11 @@
 if (!require("pacman")) { install.packages("pacman") }
 pacman::p_load(rtweet, httpuv, dplyr, lubridate, glue)
 
+token <- create_token(
+  app = "extract_data_with_r",
+  consumer_key = api_key,
+  consumer_secret = api_secret_key)
+
 data <- search_tweets("#datosdemiercoles", n = 1000, type = "recent", retryonratelimit = TRUE, include_rts = TRUE)
 
 assign(glue("semana", week(Sys.Date() - days(1))),
@@ -13,4 +18,4 @@ assign(glue("semana", week(Sys.Date() - days(1))),
 try(dir.create("meta"))
 try(dir.create(glue("meta/{year(Sys.Date())}")))
 
-save(semana44, file = glue("meta/{year(Sys.Date())}/{Sys.Date() - days(1)}.rda"), compress = "xz")
+save(semana50, file = glue("meta/{year(Sys.Date())}/{Sys.Date() - days(1)}.rda"), compress = "xz")
